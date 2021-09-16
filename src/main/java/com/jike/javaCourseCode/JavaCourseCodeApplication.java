56ce30ed6570db708ce6333285d537961c0e69a3
rongdanfeng <1179617519@qq.com>
2021-09-16 12:36:21 +0800

    add  init  projeckot

---
+++ src/main/java/com/jike/javaCourseCode/JavaCourseCodeApplication.java
@@ -0,0 +1,13 @@
+package com.jike.javaCourseCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourseCodeApplication.class, args);
+	}
+
+}
